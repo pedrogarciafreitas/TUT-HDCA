30e42656227eb74991fee482a140a5f73148adfb
Pekka Astola <pekka.astola@tut.fi>
2017-12-20 12:52:55 +0200

    generalized warping with hor,ver disparity as input

--- warpView/warpView.cpp
+++ warpView/warpView.cpp
@@ -11,18 +11,20 @@ int main(const int argc, const char** argv) {
 
 	int nr, nc, ncomponents, nvc;
 
-	int ii0 = atof(argv[3]), jj0 = atof(argv[4]), ii1 = atoi(argv[5]), jj1 = atoi(argv[6]);
+	//float ii0 = atof(argv[3]), jj0 = atof(argv[4]), ii1 = atof(argv[5]), jj1 = atof(argv[6]);
 
 	aux_read_header_file(&nr, &nc, &ncomponents, &nvc, argv[1]);
-
 
 	/* reference */
 	unsigned short *AA1 = new unsigned short[nr*nc*ncomponents];
 	aux_read_file_uint16(nr, nc, ncomponents, argv[1], AA1);
 
-	/* quantized disparity */
-	float *quantDM = new float[nr*nc];
-	aux_read_file_float(nr, nc, 1, argv[2], quantDM);
+	/* disparity */
+	float *DM_ROW = new float[nr*nc];
+	aux_read_file_float(nr, nc, 1, argv[2], DM_ROW);
+
+	float *DM_COL = new float[nr*nc];
+	aux_read_file_float(nr, nc, 1, argv[3], DM_COL);
 	
 	
 	int *ROWS = new int[nr*nc];
@@ -48,20 +50,20 @@ int main(const int argc, const char** argv) {
 	
 	for (int ij = 0; ij < nr*nc; ij++)
 	{
-		float disp0 = quantDM[ij];
+		float disp0 = abs( DM_COL[ij] ) + abs( DM_ROW[ij] );
 
 		int ix = ij % nr; //row
 		int iy = (ij - ix) / nr; //col
 
-		int iynew = iy + (int)round(((float)(ii1 - ii0))*quantDM[ij] + (float)COLS[ij]);
-		int ixnew = ix + (int)round(((float)(jj1 - jj0))*quantDM[ij] + (float)ROWS[ij]);
+		int iynew = iy + (int)round( DM_COL[ij] );
+		int ixnew = ix + (int)round( DM_ROW[ij] );
 
 		if (iynew >= 0 && iynew<nc && ixnew >= 0 && ixnew < nr){
 			int indnew = ixnew + iynew*nr;
 			if (DispTarg[indnew] < disp0){
 				DispTarg[indnew] = disp0;
-				DispTarg_col[indnew] = iynew - iy;
-				DispTarg_row[indnew] = ixnew - ix;
+				DispTarg_col[indnew] = iynew - iy; // warped horizontal disparity
+				DispTarg_row[indnew] = ixnew - ix; // warped vertical disparity
 				Warped[indnew] = AA1[ij];
 				Warped[indnew + nr*nc] = AA1[ij+nr*nc];
 				Warped[indnew + 2*nr*nc] = AA1[ij+2*nr*nc];
@@ -70,33 +72,33 @@ int main(const int argc, const char** argv) {
 
 	}
 	
-	aux_write_header_file(nr, nc, 3, 1, argv[9]);
+	aux_write_header_file(nr, nc, 3, 1, argv[4]);
 
-	FILE *f_file = fopen(argv[9], "a+b");
+	FILE *f_file = fopen(argv[4], "a+b");
 
 	fwrite(Warped, sizeof(unsigned short), nr*nc*3, f_file);
 
 	fclose(f_file);
 
-	aux_write_header_file(nr, nc, 1, 1, argv[10]);
+	aux_write_header_file(nr, nc, 1, 1, argv[5]);
 
-	f_file = fopen(argv[10], "a+b");
+	f_file = fopen(argv[5], "a+b");
 
 	fwrite(DispTarg, sizeof(float), nr*nc, f_file);
 
 	fclose(f_file);
 
-	aux_write_header_file(nr, nc, 1, 1, argv[11]);
+	aux_write_header_file(nr, nc, 1, 1, argv[6]);
 
-	f_file = fopen(argv[11], "a+b");
+	f_file = fopen(argv[6], "a+b");
 
 	fwrite(DispTarg_col, sizeof(float), nr*nc, f_file);
 
 	fclose(f_file);
 
-	aux_write_header_file(nr, nc, 1, 1, argv[12]);
+	aux_write_header_file(nr, nc, 1, 1, argv[7]);
 
-	f_file = fopen(argv[12], "a+b");
+	f_file = fopen(argv[7], "a+b");
 
 	fwrite(DispTarg_row, sizeof(float), nr*nc, f_file);
 
